c08381e8d596d2c2bdf46f17479b6f6947cb8505
Margarita Shakhova <shakhova.margarita@gmail.com>
2017-07-03 13:27:23 -0700

    Implement environment audit reports
    Add notifications about environment events that are required for
    tracking. These are AMQP notifications and oslo.messaging library
    is used for sending them.
    
    The follow event types are provided:
    
     - environment.deploy.end
       This event is issued on successful finish of environment deployment,
       provides general information about environment and also deployment
       start and finish times
    
     - environment.delete.end
       This event is issued on environment delete (and abandon as well)
    
     - environment.exists
       This is a period event, it's issued for every existing environment
       that has successful deployments
    
    There are 2 new configuration options controlling these notifications:
    
     - stats.env_audit_period
       Controls how often to send environment.exists notification, by
       it's once per hour (60 minutes)
    
     - env_audit_enabled
       Allows to completely disable environment-related notifications. By
       default notifications are enabled.
    
    Change-Id: I8dee2456b7ccab7c0c167aa21abb9710959ebb30

--- murano/tests/unit/base.py
+++ murano/tests/unit/base.py
@@ -41,3 +41,5 @@ class MuranoWithDBTestCase(MuranoTestCase):
         self.override_config('connection', "sqlite://", group='database')
         db_api.setup_db()
         self.addCleanup(db_api.drop_db)
+
+        self.override_config('env_audit_enabled', False, group='stats')
